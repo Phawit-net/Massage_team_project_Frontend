cdee024ab0549e34d6c003e9e42494932d4e7045
Apichai123 <rusefer@gmail.com>
2020-01-13 19:56:09 +0700

    install

---
+++ src/components/UserProfile/UserInformation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserInformation extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
