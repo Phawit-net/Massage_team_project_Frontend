35214af26c9ff88c66ac33b71da6a8ec73965458
panuwat33586 <panuwat33586@gmail.com>
2020-01-13 10:59:20 +0700

    adding footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css'
+import {Row} from 'antd'
 import Header from './components/Generals/Header/Header'
+import Footer from './components/Generals/Footer/Footer'
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Header/>
-      </div>
+      <>
+        <Row>
+          <Header/>
+        </Row>
+        <Row>
+          content
+        </Row>
+        <Row>
+          <Footer/>
+        </Row>   
+      </>
     )
   }
 }
