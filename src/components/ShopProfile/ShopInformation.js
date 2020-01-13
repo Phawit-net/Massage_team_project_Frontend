69d702c294a15e478909e4d6e1288156f1545e95
Apichai123 <rusefer@gmail.com>
2020-01-13 19:56:09 +0700

    install

---
+++ src/components/ShopProfile/ShopInformation.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ShopInformation extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
