f2ca2b8a662f380f3cc070b5ecd15583f2e88855
panuwat33586 <panuwat33586@gmail.com>
2020-01-13 10:01:02 +0700

    update header component

--- src/components/Generals/Header/Header.js
+++ src/components/Generals/Header/Header.js
@@ -37,7 +37,7 @@ export default class Header extends Component {
                                 <Menu.Item className={styles.menuoption}>Login</Menu.Item>
                             </Menu>
                             <Button id={styles.drawermenu} icon='menu-unfold' size='large' ghost onClick={this.showDrawer}/>
-                            <Drawer
+                            <Drawer 
                                 title="Menu"
                                 placement="right"
                                 closable={false}
