40d02b04317c78d3d71d25e341d5bf87dbcd5fa9
Apichai123 <rusefer@gmail.com>
2020-01-13 20:39:07 +0700

    Edit Charts

--- src/components/UserProfile/ServiceUsage.js
+++ src/components/UserProfile/ServiceUsage.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import Chart from 'react-apexcharts'
 
 export default class ServiceUsage extends Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          options: {},
+          series: [44, 55, 41, 17, 15],
+          labels: ["A", "B", "C", "D", "E"]
+        };
+      }
     render() {
         return (
-            <div>
-                
-            </div>
+            <div >
+            <Chart
+              options={this.state.options}
+              series={this.state.series}
+              type="donut"
+              width="380"
+            />
+          </div>
         )
     }
 }
