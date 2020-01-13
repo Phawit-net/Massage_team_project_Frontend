eca560929fee97f80f17ac31dd56096e7d2bad23
Phawit-net <neztle.net@gmail.com>
2020-01-13 23:10:54 +0700

    shopCard pagination

--- src/components/Generals/Shop/ShopList.js
+++ src/components/Generals/Shop/ShopList.js
@@ -2,35 +2,26 @@ import React, { Component } from "react";
 import ShopCard from "./ShopCard";
 import Axios from 'axios';
 
+
 export default class ShopList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      shopList : [{
-        id:1,
-        name:'asdas'
-      },{
-        id:2,
-        name:'qweqwe'
-      }]
+      shopList : []
     };
   }
 
-  componentDidMount(){
-    Axios.get("http://localhost:8080/shops")
-      .then(result => {
-        this.setState({
-          shopList: result.data
-        });
-      },
-        console.log(this.state)
-      )
+  async componentDidMount(){
+    const result = await Axios.get(`http://localhost:8080/shops`)
+    this.setState({ shopList: result.data })
+    console.log(this.state.shopList)
   }
 
   render() {
     return (
       <div>
-        <ShopCard shopList={this.state.shopList}/>
+        <ShopCard 
+          shopList = {this.state.shopList}/>
       </div>
     );
   }
