0498a1bbb702e36bb1b2a4e6c35016e99e96fe84
bebiiez <natthalada.p@gmail.com>
2020-01-15 18:17:47 +0700

    Edit: bookingModal-redux

--- src/components/ShopDetails/BookingModal.js
+++ src/components/ShopDetails/BookingModal.js
@@ -1,26 +1,52 @@
 import React, { Component } from 'react'
-import { Row, Col, InputNumber, Form, DatePicker, TimePicker, Button } from 'antd'
+import { Row, Col, InputNumber, Form, DatePicker, TimePicker, Button, Modal, Avatar, message } from 'antd'
 import moment from 'moment';
+
+import { withRouter } from 'react-router-dom'
+import Axios from '../../config/axios.setup'
+import { connect } from 'react-redux';
+import { addBooking } from "../../redux/actions/actions";
 import styles from './BookingModal.module.css'
 
-export class BookingModal extends Component {
+class BookingModal extends Component {
   state = {
-    serviceList: {
-
-      serviceName: "Full Body Thai Yoga massage", serviceDescription: " In this unique healing system of Thai Yoga Massage, also called Nuad Boran, the practitioner guides the client through a series of yoga postures, while palming and thumbing along the body’s energy (‘Sen’) lines and pressure points.",
-      serviceProfilePic: "https://cdn.spafinder.com/2015/10/thai-massage-1.jpg", time: "", price: 1000, service_id: "1"
-
-    },
+    serviceList: [],
+    shopList: [],
     personValue: 1,
     dateValue: '',
     startValue: '',
     endValue: '',
+    bookingvisible: false
   }
 
-  handleAdd1hr(start,startString) {
+  componentDidMount() {
+    let targetServiceID = 1;
+    Axios.get(`http://localhost:8080/servicesDetail?id=${targetServiceID}`).then(
+      result => {
+        this.setState({
+          serviceList: result.data,
+          shopList: result.data.shop
+        });
+      }
+    );
+  }
+
+  showBookingModal = () => {
+    this.setState({
+      bookingvisible: true,
+    });
+  };
+
+  hideBookingModal = e => {
+    this.setState({
+      bookingvisible: false,
+    });
+  };
+
+  handleAdd1hr(start, startString) {
     this.setState({ startValue: startString })
     this.props.form.setFieldsValue({
-      endTime: moment(this.add1Hour(start),'HH:mm'),
+      endTime: moment(this.add1Hour(start), 'HH:mm'),
     });
   }
 
@@ -31,33 +57,25 @@ export class BookingModal extends Component {
     return onehour
   }
 
-  handleSubmit = e => {
-    e.preventDefault();
+  handleSubmit(service) {
     this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      //   Axios.post('http://localhost:8080/', { 
-      //               name: values.person,
-      //               lastname: values.date,
-      //               startTime: values.startTime,
-      //               endTime: values.endTime,
-      //               price: values.person * this.state.serviceList.price
-      //             })
-      //               .then(result => {
-      //                 console.log(result)
-      //               })
-      //               .catch(err => {
-      //                 console.error(err)
-      //               })
-      //             this.props.form.resetFields()
+      let person = values.person
+      let date = this.state.dateValue
+      let startTime = this.state.startValue
+      let endTime = this.state.endValue
+      let price = person * this.state.serviceList.price
+      
+      if (service && person && date && startTime && endTime && price) {
+        this.props.addBooking(service, person, date, startTime, endTime, price);
+        this.props.history.push('/payment')
+      }else{
+        message.error('Please input your booking information');
       }
-    });
+    })
   };
 
 
   render() {
-    console.log('Start', this.state.startValue)
-    console.log('End', this.state.endValue)
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
@@ -85,102 +103,120 @@ export class BookingModal extends Component {
     let service = this.state.serviceList
 
     return (
-      <Row className={styles.borderBox}>
-        <Col span={24} style={{ width: '100%', height: '100%' }}>
+      <>
+        <Button type='link' className={styles.button} onClick={() => this.showBookingModal()}>Booking</Button>
+        <Modal
+          width={800}
+          visible={this.state.bookingvisible}
+          onCancel={this.hideBookingModal}
+          footer={null}
+        >
+          <Row className={styles.borderBox}>
+            <Col span={24} style={{ width: '100%', height: '100%' }}>
 
-          <Row className={styles.borderBoxSmall}>
-            <Col span={8} style={{ padding: '5px' }} >
-              <img src={service.serviceProfilePic} alt="" style={{ objectFit: 'cover', height: '200px' }} />
-            </Col>
-
-            <Col span={16}>
-              <Row>
-                <Col span={16} style={{ color: '#926F3B' }}>
-                  <h3>{service.serviceName} ({this.state.personValue} person)</h3>
+              <Row className={styles.borderBoxSmall} type='flex' justify='space-around'>
+                <Col xs={24} lg={9} xl={8} style={{textAlign: 'center'}}>
+                  <Avatar src={service.serviceProfilePic} alt="" shape='square' size={200} style={{ margin: '5px' }} />
                 </Col>
-                <Col span={8}>
-                  <Row><h4>Arunmanee Massage & Spa</h4></Row>
-                  <Row>{this.state.dateValue} {this.state.startValue}-{this.state.endValue}</Row>
+
+                <Col xs={24} lg={14} xl={16}>
+                  <Row>
+                    <Col span={16} style={{ color: '#926F3B' }}>
+                      <h3>{service.serviceName}</h3>
+                      <h5>({this.state.personValue} person)</h5>
+                    </Col>
+                    <Col span={8}>
+                      <Row><h4>{this.state.shopList.shopName}</h4></Row>
+                      <Row><h5>Time: </h5><h6>{this.state.startValue ? `${this.state.startValue}-${this.state.endValue}` : undefined}</h6></Row>
+                      <Row><h5>Date: </h5><h6>{this.state.dateValue}</h6></Row>
+                    </Col>
+                  </Row>
+                  <Row style={{ padding: '10px' }}><h5>{service.serviceDescription}</h5></Row>
+                </Col>
+
+              </Row>
+
+              <Row style={{ padding: '5px' }}>
+                <Col>
+
+                  <Row><h1>Booking Information</h1></Row>
+                  <Form {...formItemLayout} labelAlign='left'>
+                    <Row>
+                      <Form.Item label="Number of user:">
+                        {getFieldDecorator('person', {
+                          initialValue: 1,
+                          rules: [{ required: true, message: 'Please input number of user' }],
+                        })(<InputNumber
+                          style={{ width: '200px' }}
+                          min={1}
+                          max={10}
+                          formatter={value => `${value} person`}
+                          parser={value => value.replace(' person', '')}
+                          onChange={(value) => { this.setState({ personValue: value }) }} />)}
+                      </Form.Item>
+                    </Row>
+
+                    <Row>
+                      <Form.Item label="Date:">
+                        {getFieldDecorator('date', {
+                          rules: [{ type: 'object', required: true, message: 'Please select date of booking' }]
+                        })(<DatePicker onChange={(date, dateString) => { this.setState({ dateValue: dateString }) }} />)}
+                      </Form.Item>
+                    </Row>
+
+                    <Row>
+                      <Form.Item label="Start time:">
+                        {getFieldDecorator('startTime', {
+                          rules: [{ type: 'object', required: true, message: 'Please select time of booking' }]
+                        })(<TimePicker format="HH:00"
+                          onChange={(start, startString) => this.handleAdd1hr(start, startString)} />)}
+                      </Form.Item>
+                    </Row>
+
+                    <Row>
+                      <Form.Item label="End time:">
+                        {getFieldDecorator('endTime', {
+                          initialValue: moment(this.state.endValue, 'HH:mm'),
+                          rules: [{ type: 'object' }]
+                        })(<TimePicker disabled format='HH:mm' />)}
+                      </Form.Item>
+                    </Row>
+
+                    <Row>
+                      <Form.Item label="Total price:">
+                        <Col>{service.price * this.state.personValue} Bath</Col>
+                      </Form.Item>
+                    </Row>
+
+                    <Row type='flex' justify='center'>
+                      <Form.Item style={{ margin: 0 }} {...tailFormItemLayout}>
+                        <Col span={12} width="100%">
+                          <Button style={{ float: 'right', margin: '3px' }} onClick={this.hideBookingModal}>cancle</Button>
+                        </Col>
+                        <Col span={12} width="100%">
+                          <Button htmlType="submit" style={{ float: 'left', margin: '3px' }}
+                            className={styles.button} onClick={() => this.handleSubmit(service)}
+                          >comfirm</Button>
+                        </Col>
+                      </Form.Item>
+                    </Row>
+
+                  </Form>
                 </Col>
               </Row>
-              <Row style={{ paddingTop: '15px' }}>{service.serviceDescription}</Row>
             </Col>
 
           </Row>
-
-          <Row style={{ padding: '5px' }}>
-            <Col>
-
-              <Row><h1>Booking Information</h1></Row>
-              <Form {...formItemLayout} labelAlign='left' onSubmit={this.handleSubmit}>
-                <Row>
-                  <Form.Item label="Number of user:">
-                    {getFieldDecorator('person', {
-                      initialValue: 1,
-                      rules: [{ required: true, message: 'Please input number of user' }],
-                    })(<InputNumber
-                      style={{ width: '200px' }}
-                      min={1}
-                      max={10}
-                      formatter={value => `${value} person`}
-                      parser={value => value.replace(' person', '')}
-                      onChange={(value) => { this.setState({ personValue: value }) }} />)}
-                  </Form.Item>
-                </Row>
-
-                <Row>
-                  <Form.Item label="Date:">
-                    {getFieldDecorator('date', {
-                      rules: [{ type: 'object', required: true, message: 'Please select date of booking' }]
-                    })(<DatePicker onChange={(date, dateString) => { this.setState({ dateValue: dateString }) }} />)}
-                  </Form.Item>
-                </Row>
-
-                <Row>
-                  <Form.Item label="Start time:">
-                    {getFieldDecorator('startTime', {
-                      rules: [{ type: 'object', required: true, message: 'Please select time of booking' }]
-                    })(<TimePicker format="HH:00" 
-                      // onChange={(start, startString) => { this.setState({ startValue: startString }) }}
-                      onChange={(start,startString) => this.handleAdd1hr(start,startString)} />)}
-                  </Form.Item>
-                </Row>
-
-                <Row>
-                  <Form.Item label="End time:">
-                    {getFieldDecorator('endTime', {
-                      initialValue: moment(this.state.endValue, 'HH:mm'),
-                      rules: [{ type: 'object' }]
-                    })(<TimePicker disabled format='HH:mm' />)}
-                  </Form.Item>
-                </Row>
-
-                <Row>
-                  <Form.Item label="Total price:">
-                    <Col>{service.price * this.state.personValue} Bath</Col>
-                  </Form.Item>
-                </Row>
-
-                <Row>
-                  <Form.Item style={{ margin: 0 }} {...tailFormItemLayout}>
-                    <Col span={12}>
-                      <Button style={{ float: 'right', margin: '3px' }}>cancle</Button>
-                    </Col>
-                    <Col span={12}>
-                      <Button htmlType="submit" style={{ float: 'left', margin: '3px' }} className={styles.button}>comfirm</Button>
-                    </Col>
-                  </Form.Item>
-                </Row>
-
-              </Form>
-            </Col>
-          </Row>
-        </Col>
-
-      </Row>
+        </Modal>
+      </>
     )
   }
 }
 
 const FormBookingModal = Form.create({ name: 'bookingService' })(BookingModal);
-export default FormBookingModal
+
+const mapDispatchToProps = {
+  addBooking: addBooking,
+};
+
+export default connect(null, mapDispatchToProps)(withRouter(FormBookingModal));
