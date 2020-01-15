c05f001d2a2aef5fc9b886b7da00ad3744650e1a
panuwat33586 <panuwat33586@gmail.com>
2020-01-15 17:27:13 +0700

    create upload preview function in payment.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react'
-import ShopList from '../components/Generals/Shop/ShopList'
-import { Carousel,Row, Typography, Col} from 'antd';
-import Axios from 'axios';
-
-const { Text } = Typography;
-
 export default class Home extends Component {
   constructor(props) {
     super(props);
@@ -29,26 +23,6 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <Carousel autoplay className='catMenu'>
-                  <div>
-                    <img style={{ width: '100%', height: '100%' }}src="/humans.jpg" alt="img1" />
-                  </div>
-                  <div>
-                    <img style={{ width: '100%', height: '100%' }} src="/massage.jpg" alt="img2" />
-                  </div>
-                  <div>
-                    <img style={{ width: '100%', height: '100%' }} src="/thai_massage_twickenham.jpg" alt="img3" />
-                  </div>
-                </Carousel>
-                <div style={{backgroundColor:'#f1e6b2', opacity: '0.8' , padding:'60px' ,fontSize:'20px'}}>
-                  <Row style={{display:'flex',justifyContent:'center'}}>
-                    Heaven For Thai Massage Lover
-                  </Row>
-                  <Row style={{display:'flex',justifyContent:'center'}}>
-                    A place where Thai massage from all over Thailand meet Thai massge lover
-                  </Row>
-                </div>
-                <ShopList shopList={this.state.shopList} callbackFromParent={this.myCallback}/>
             </div>
         )
     }
