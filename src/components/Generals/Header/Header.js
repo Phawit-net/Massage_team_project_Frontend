3379ecbdc1fbfaf2dae1dd320143393560f53be7
panuwat33586 <panuwat33586@gmail.com>
2020-01-17 14:37:12 +0700

    add link to home to logo

--- src/components/Generals/Header/Header.js
+++ src/components/Generals/Header/Header.js
@@ -74,7 +74,7 @@ class Header extends Component {
             <Card style={{ backgroundColor: '#f1e6b2', opacity: '0.8' }} bodyStyle={{ padding: '0' }}>
                 <Row type='flex' justify='center' align='middle'>
                     <Col xs={19} md={5} lg={10} xl={10}>
-                        <img src={Logo} alt='Logo' style={{ width: '150px', height: '150px' }} />
+                   <Link to ='/home'><img src={Logo} alt='Logo' style={{ width: '150px', height: '150px' }} /></Link>     
                     </Col>
                     <Col xs={5} md={19} lg={12} xl={10}>
                         <Row>
