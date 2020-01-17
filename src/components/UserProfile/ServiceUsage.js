9c8d1011b777a51f4ed415f75d4c9d762d56e4b5
Apichai123 <rusefer@gmail.com>
2020-01-17 11:36:50 +0700

    edit ServiceUsage.js

--- src/components/UserProfile/ServiceUsage.js
+++ src/components/UserProfile/ServiceUsage.js
@@ -1,26 +1,88 @@
-import React, { Component } from 'react'
-// import Chart from 'react-apexcharts'
-
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+import { Row, Col, Form } from "antd";
+import Axios from "../../config/axios.setup";
 export default class ServiceUsage extends Component {
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-          options: {},
-          series: [44, 55, 41, 17, 15],
-          labels: ["A", "B", "C", "D", "E"]
-        };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: {labels: [],plotOptions: {
+        //-----------------
+        pie: {
+          donut: {
+            labels: {
+              show: true,
+              total: {
+                showAlways: true,
+                show: true
+              }
+            }
+          }
+        }
+        //-----------------
+      }},
+      series: [],
+      labels: [],
+    };
+  }
+
+  componentDidMount(){
+    Axios.get('/getShopUserUsage')
+   .then(result => {
+     let shopnames=[],shopcount=[],showShopname={labels: []}
+     result.data.map(function(x) {
+      shopnames.push(x.shopName)
+      showShopname.labels.push("Shop - " + x.shopName)
+      shopcount.push(x.count)
+    });
+    console.log(shopnames)
+    console.log(shopcount)
+    this.setState({
+      labels:shopnames,
+      series:shopcount,
+      options:showShopname
+    })
+
+   })
+   .catch(err => {
+     console.error(err);
+   })
+   
+
+}
+
+
+  render() {
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 16 }
       }
-    render() {
-        return (
-            <div >
-            {/* <Chart
-              options={this.state.options}
-              series={this.state.series}
-              type="donut"
-              width="380"
-            /> */}
-          </div>
-        )
-    }
+    };
+    return (
+      <Row type="flex" justify="center" align="top">
+        <Col span={9}>
+          <Row type="flex" justify="start">
+            <Col style={{ fontSize: "25px" }}>Service Usage</Col>
+          </Row>
+          <Row>
+            <Form {...formItemLayout}>
+              <Chart
+                options={this.state.options}
+                series={this.state.series}
+                type="donut"
+                //type="pie"
+                width="380"
+              />
+            </Form>
+          </Row>
+        </Col>
+      </Row>
+    );
+  }
 }
