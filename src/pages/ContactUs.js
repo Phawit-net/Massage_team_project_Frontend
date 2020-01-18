22571530f1b639f0a8dd20d2ec77543df54541f9
panuwat33586 <panuwat33586@gmail.com>
2020-01-18 22:57:43 +0700

    link contact us page with backend

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react'
-import { Row, Col, Form, Input, Select, Button, Divider, Card } from 'antd'
+import { Row, Col, Form, Input, Select, Button, Divider, Card,message } from 'antd'
 import testmodel from '../picture/test model.png'
 import styles from './ContactUs.module.css'
 import aboutus from '../picture/about us.jpg'
-const { TextArea } = Input;
+import Axios from '../config/axios.setup'
 const { Option } = Select;
-export default class ContactUs extends Component {
+
+class ContactUs extends Component {
+handleSubmit(e){
+    e.preventDefault()  
+    this.props.form.validateFields((err,values)=>{
+        if(!err){
+            Axios.post('/contact',{
+                sender:values.sender,
+                receiver:values.receiver,
+                title:values.title,
+                message:values.message
+            })
+            .then(()=>{
+                message.success(`your mail has been sent to ${values.receiver}`)
+            })
+            .catch(err=>{
+                message.error('something wrong')
+            })
+        }
+    })
+}
     render() {
+        const { getFieldDecorator } = this.props.form;
         return (
             <>
                 <Row style={{ marginTop: '150px' }} id='About this project'>
                     <Row type='flex' style={{ height: '700px' }} >
-                        <img src={aboutus} style={{ height: '100%', width: '100%' }} />
+                        <img src={aboutus} style={{ height: '100%', width: '100%' }} alt='aboutus top pic' />
                     </Row>
                     <Row style={{ marginLeft: '10%', marginRight: '10%', marginTop: '20px' }} type='flex' align='middle' gutter={[32, 0]} >
                         <Col span={12}>
@@ -50,17 +71,36 @@ export default class ContactUs extends Component {
                                 <h1>Contact us via email</h1>
                             </Row>
                             <Row type='flex' justify='start'>
-                                <Form layout='vertical'>
+                                <Form layout='vertical' onSubmit={(e)=>this.handleSubmit(e)}>
+                                <Form.Item label='Your Email'>
+                                        {getFieldDecorator('sender',
+                                        {rules: [{ required: true, message: 'Please input your email' }]}
+                                        )(
+                                       <Input style={{ width: '300px' }} placeholder='please input your email'/>
+                                        )}
+                                    </Form.Item>
                                     <Form.Item label='Email'>
+                                        {getFieldDecorator('receiver',
+                                        {rules: [{ required: true, message: 'Please select email' }]}
+                                        )(
                                         <Select style={{ width: '300px' }} placeholder='please select an email'>
                                             <Option value='panuwat33586@gmail.com'>A (panuwat33586@gmail.com)</Option>
                                         </Select>
+                                        )}
                                     </Form.Item>
                                     <Form.Item label='Title'>
+                                    {getFieldDecorator('title',
+                                        {rules: [{ required: true, message: 'Please input title' }]}
+                                        )(
                                         <Input style={{ width: '300px' }} placeholder='please input title' />
+                                        )}   
                                     </Form.Item>
                                     <Form.Item label='Message'>
-                                        <TextArea style={{ width: '400px' }} autoSize={{ minRows: 5, maxRows: 5 }} placeholder='please input message that you want to inform ' />
+                                    {getFieldDecorator('message',
+                                        {rules: [{ required: true, message: 'Please input your message' }]}
+                                        )(
+                                         <Input.TextArea style={{ width: '400px' }} autoSize={{ minRows: 5, maxRows: 5 }} placeholder='please input message that you want to inform ' />
+                                        )}                       
                                     </Form.Item>
                                     <Form.Item>
                                         <Button type="primary" htmlType="submit" className={styles.button}>
@@ -85,3 +125,5 @@ export default class ContactUs extends Component {
         )
     }
 }
+
+export default Form.create()(ContactUs)
