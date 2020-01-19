a794210a0660548eeee70fdf2cd1c8f60f9d8e8b
Phawit-net <neztle.net@gmail.com>
2020-01-20 05:12:12 +0700

    decorate home

--- src/components/Generals/Shop/ShopCard.js
+++ src/components/Generals/Shop/ShopCard.js
@@ -3,7 +3,7 @@ import { Row, Typography, Col, Button, List } from "antd";
 import styles from "./ShopCard.module.css";
 import { Link } from 'react-router-dom'
 
-const { Text } = Typography;
+const { Paragraph,Text } = Typography;
 
 export default class ShopCard extends Component {
   render() {
@@ -11,28 +11,36 @@ export default class ShopCard extends Component {
     return (
       <div>
         <List.Item key={item.id}>
-          <Row type="flex" style={{ border: "2px solid #D8AE47", margin: "10px", padding: "10px" }} justify='center'>
-            <Col md={4} sm={12} xs={24}>
+          <Row style={{ border: "2px solid #D8AE47", margin: "10px" }}>
+            <Row type='flex' justify ='center'>
+            <Col style={{position: 'absolute',width:'100%',height:'100%'}}>
+              <img src='/pattern4.png'  style={{position: 'absolute'}}/>           
+              <img src='/pattern5.png'  style={{position: 'absolute',right:'0%'}}/>
+              <img src='/pattern4.png' style={{position: 'absolute', transform: 'scaleY(-1)',bottom:'0%'}}/>           
+              <img src='/pattern5.png' style={{position: 'absolute', transform: 'scaleY(-1)',right:'0%',bottom:'0%'}}/>
+            </Col>
+            <Col md={4} sm={16} xs={24} style={{padding:'30px 10px 30px 30px'}} className={styles.imgContent}>
               <img
                 style={{ width: "100%" }}
                 src= {`http://localhost:8080/${item.shopProfilePic}`}
               />
             </Col>
-            <Col md={20} sm={24} xs={24} style={{ paddingLeft: '20px' }}>
+            <Col md={20} sm={24} xs={24} style={{padding:'30px 30px 30px 10px'}}className={styles.content} >
               <Row>
                 <Text strong style={{ fontSize: "1.6em" }}>
                   {item.shopName}
                 </Text>
               </Row>
               <Row>
-                <Text>
+                <Paragraph ellipsis={{ rows: 3, expandable: true }} >
                   {item.shopDescription}
-                </Text>
+                </Paragraph>
               </Row>
-              <Row style={{ display: 'flex', justifyContent: 'flex-end' }}>
+              <Row style={{display: 'flex', justifyContent: 'flex-end' ,alignItems:'flex-end'}} className={styles.ButtonRow}>
                 <Button type='primary' className={styles.Button}><Link to={`/shopdetails?id=${item.id}`}>View Shop</Link></Button>
               </Row>
             </Col>
+            </Row>
           </Row>
         </List.Item>
       </div>
