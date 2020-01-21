9092b2df676a519949f60558b064c9d0f76bd1e3
Phawit-net <neztle.net@gmail.com>
2020-01-22 04:43:49 +0700

    shop detail

--- src/pages/ShopDetails.js
+++ src/pages/ShopDetails.js
@@ -1,73 +1,67 @@
-import React, { Component } from 'react'
-import { Row, Col } from 'antd'
-import Axios from '../config/axios.setup'
-import { withRouter } from 'react-router-dom'
-import ServiceList from '../components/ShopDetails/ServiceList'
+import React, { Component } from "react";
+import { Row, Col,Typography } from "antd";
+import Axios from "../config/axios.setup";
+import { withRouter } from "react-router-dom";
+import ServiceList from "../components/ShopDetails/ServiceList";
+import styles from "./ShopDetails.module.css";
+const { Text } = Typography;
 
 class ShopDetails extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       page: 1,
       shopName: "",
       shopProfilePic: "",
-      servicesList: [],
-    }
+      servicesList: []
+    };
   }
 
   async componentDidMount() {
-    let targetShopId = this.props.history.location.search.slice(4)
-    const result = await Axios.get(`http://localhost:8080/shop?id=${targetShopId}`)
+    let targetShopId = this.props.history.location.search.slice(4);
+    const result = await Axios.get(
+      `http://localhost:8080/shop?id=${targetShopId}`
+    );
     this.setState({
       shopName: result.data.shopName,
       shopProfilePic: result.data.shopProfilePic,
-      servicesList: result.data.services,
-    })
+      servicesList: result.data.services
+    });
   }
 
   render() {
     return (
       <div>
-        <Row style={{ marginTop: "200px" }}>
-          <Col>
-            <Row
-              style=
-              {{
-                backgroundImage: `url(${Axios.defaults.baseURL}/${this.state.shopProfilePic})`,
-                backgroundRepeat: "no-repeat",
-                backgroundAttachment: "fixed",
-                backgroundSize: "cover",
-              }}
-            >
-              <Col span={12} >
-                <img
-                  style=
-                  {{
-                    borderRadius: "50%",
-                    // borderRadius: "8px",
-                    border: "1px solid #000",
-                    width: "150px",
-                    height: "auto",
-                    float: "right"
-                  }}
-
-                  src={`${Axios.defaults.baseURL}/${this.state.shopProfilePic}`} />
-              </Col>
-              <Col span={12}>
-                <h1>{this.state.shopName}</h1>
-              </Col>
-            </Row>
+        <Row style={{ marginTop: "50px" }}>
+          <Col span={24}>
+            <div className ={styles.shopName} style={{position:'absolute' ,right:'16.7%',top:'25.9%',backgroundColor:'#f1e0b9',opacity:'0.4',width:'275px',height:'90px'}}></div>
+            <div className ={styles.shopName} style={{position:'absolute' ,right:'17.5%',top:'27%',backgroundColor:'#FFF',opacity:'0.2',width:'275px',height:'90px'}}></div>
+            <div className ={styles.shopName} style={{position:'absolute' ,right:'17%',top:'26.3%',width:'275px',height:'90px' , display:'flex' , justifyContent:'center',alignItems:'center'}}>
+              <Text className={styles.font} style={{fontSize:'50px'}}>{this.state.shopName.charAt(0).toUpperCase() + this.state.shopName.slice(1)}</Text>
+            </div>
+            <img src="nuadthaiRoom.jpg" style={{width:'100%'}}/>
+          </Col>
+          <Col span={24}>
+            <div style={{ borderTop: '5px solid #855f3e' }}></div>
           </Col>
         </Row>
+        <Row style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+          <Col>
+          <img style={{width: "150px",height: "auto"}} src={`${Axios.defaults.baseURL}/${this.state.shopProfilePic}`}/>
+          </Col>
+          <Col>
+            <h1>{this.state.shopName}</h1>
+          </Col>
 
-        <Row  >
-          <Col sm={{ span: 24, offset: 0 }} md={{ span: 22, offset: 1 }} >
+        </Row>
+
+        <Row>
+          <Col sm={{ span: 24, offset: 0 }} md={{ span: 22, offset: 1 }}>
             <ServiceList servicesList={this.state.servicesList} />
           </Col>
         </Row>
       </div>
-
-    )
+    );
   }
 }
 
