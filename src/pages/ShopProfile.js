a0e200e602724bee9c1bc438a95fec778dc2dff9
Bhornumnard <tak_25908@hotmail.com>
2020-01-21 14:23:23 +0700

    edit to antd

--- src/pages/ShopProfile.js
+++ src/pages/ShopProfile.js
@@ -4,6 +4,8 @@ import CreateService from '../components/ShopProfile/CreateService'
 import ServiceHistory from '../components/ShopProfile/ServiceHistory'
 import ApprovePurchase from '../components/ShopProfile/ApprovePurchase'
 import CustomerUsage from '../components/ShopProfile/CustomerUsage'
+import {Row, Col} from 'antd';
+import '../Shopprofiles.css'
 
 
 
@@ -53,17 +55,19 @@ export default class ShopProfile extends Component {
         return (
             <div style={{display:"grid", gridTemplateColumns:"25% 70%", gridGap:"5%",justifyContent:"space-around", margin:"200px 20px 20px 20px", height:"100vh"}}>
             <div style={{ width:"100%", height:"50%", position:"sticky",top:"250px"}}>
-                <div style={{display:"flex", flexFlow:"row wrap", justifyContent:"center", marginBottom:"10px"}} >
-                    <div> <img src="http://www.thaiticketmajor.com/variety/img_content/imgeditor/sky-mirror-beach.jpg" alt="picture" style={{borderRadius:"50%", width: "80px", height:"80px", marginLeft:"-50%"}}/> </div>
-                    <div style={{marginLeft:"-5%", marginTop:"8%", fontSize:"20px"}}> shop name</div>
-                </div>
-                <ul className="listUserProfile" style={{listStyle: "none", margin:"auto", width:"100%"}}>
-                    <li onClick={this.HandleShopInformation}> Shop information </li>
-                    <li onClick={this.HandleCreateService}> Create Service </li>
-                    <li onClick={this.HandleServiceHistory}> Services history</li>
-                    <li onClick={this.HandleApprovePurchase}> Approve purchase </li>
-                    <li onClick={this.HandleCustomerUsage}> Customer usage </li>
-                </ul>
+                <Row style={{display:"flex", flexFlow:"row wrap", justifyContent:"center", marginBottom:"10px"}} >
+                    <Col> <img src="http://www.thaiticketmajor.com/variety/img_content/imgeditor/sky-mirror-beach.jpg" alt="picture" style={{borderRadius:"50%", width: "80px", height:"80px", marginLeft:"-50%"}}/> </Col>
+                    <Col style={{marginLeft:"-5%", marginTop:"8%", fontSize:"20px"}}> shop name</Col>
+                </Row>
+                <Row className="listUserProfile" style={{listStyle: "none", margin:"auto", width:"100%"}}>
+                    <Col className="containerList">
+                    <Row className="list" onClick={this.HandleShopInformation}> Shop information </Row>
+                    <Row className="list" onClick={this.HandleCreateService}> Create Service </Row>
+                    <Row className="list" onClick={this.HandleServiceHistory}> Services history</Row>
+                    <Row className="list" onClick={this.HandleApprovePurchase}> Approve purchase </Row>
+                    <Row className="list" onClick={this.HandleCustomerUsage}> Customer usage </Row>
+                    </Col>
+                </Row>
             </div>
 
             <div style={{border:"1px solid black", width:"100%", height:"100%"}}>
--- src/pages/UserProfile.js
+++ src/pages/UserProfile.js
@@ -1,63 +1,64 @@
 import React, { Component } from 'react'
-import '../App.css'
+import '../UserProfile.css'
 // import PurChaseHist from '../component/UserProfile/PurchaseHistory'
 import ServiceUsage from '../components/UserProfile/ServiceUsage'
 import UserInformation from '../components/UserProfile/UserInformation'
 import PurchaseHistory from '../components/UserProfile/PurchaseHistory';
+import { Row, Col } from 'antd';
 
 export default class UserProfile extends Component {
     state = {
-        case : 1
+        case: 1
     };
 
-    HandleUserInformation = ()=> {
-        this.setState ({case:1})
+    HandleUserInformation = () => {
+        this.setState({ case: 1 })
     };
 
-    HandlePurchaseHistory= ()=>{
-        this.setState({case:2})
+    HandlePurchaseHistory = () => {
+        this.setState({ case: 2 })
     };
 
     HandleServicesUsage = () => {
-        this.setState({case:3})
+        this.setState({ case: 3 })
     };
 
-    HandleShopLink =()=>{
-        this.setState({case:4})
+    HandleShopLink = () => {
+        this.setState({ case: 4 })
     };
 
-    Display =() => {
-        switch(this.state.case){
-            case 1: return <UserInformation/>;
+    Display = () => {
+        switch (this.state.case) {
+            case 1: return <UserInformation />;
 
-            case 2: return <PurchaseHistory/>;
+            case 2: return <PurchaseHistory />;
 
-            case 3: return <ServiceUsage/>;
-                                        
-            // case 4: return <ShopLink/>;
+            case 3: return <ServiceUsage />;
+
         }
-     }
-    
+    }
+
     render() {
         return (
-            <div style={{display:"grid", gridTemplateColumns:"25% 70%", gridGap:"5%",justifyContent:"space-around", margin:"200px 20px 20px 20px", height:"100vh"}}>
-            <div style={{ width:"100%", height:"50%", position:"sticky",top:"250px"}}>
-                <div style={{display:"flex", flexFlow:"row wrap", justifyContent:"center", marginBottom:"10px"}} >
-                    <div> <img src="http://www.thaiticketmajor.com/variety/img_content/imgeditor/sky-mirror-beach.jpg" alt="picture" style={{borderRadius:"50%", width: "80px", height:"80px", marginLeft:"-50%"}}/> </div>
-                    <div style={{marginLeft:"-5%", marginTop:"8%", fontSize:"20px"}}> shop name</div>
-                </div>
-                <ul className="listUserProfile" style={{listStyle: "none", margin:"auto", width:"100%"}}>
-                    <li Onclick={this.HandleUserInformation}> User information </li>
-                    <li onClick={this.HandlePurchaseHistory}> Purchase history</li>
-                    <li onClick={this.HandleServicesUsage}> Services usage</li>
-                    <li onClick={this.HandleShopLink}> Shop link </li>
-                </ul>
-            </div>
+            <div style={{ display: "grid", gridTemplateColumns: "25% 70%", gridGap: "5%", justifyContent: "space-around", margin: "200px 20px 20px 20px", height: "100vh" }}>
+                <Row style={{ width: "100%", height: "50%", position: "sticky", top: "250px" }}>
+                    <Col>
+                        <Row style={{ display: "flex", flexFlow: "row wrap", justifyContent: "center", marginBottom: "10px" }} >
+                            <Col> <img src="http://www.thaiticketmajor.com/variety/img_content/imgeditor/sky-mirror-beach.jpg" alt="picture" style={{ borderRadius: "50%", width: "80px", height: "80px", marginLeft: "-50%" }} /> </Col>
+                            <Col style={{ marginLeft: "-5%", marginTop: "8%", fontSize: "20px" }}> shop name</Col>
+                        </Row>
+                        <Row className="listUserProfile" style={{ listStyle: "none", margin: "auto", width: "60%" }}>
+                            <Col className="list" onClick={this.HandleUserInformation}> User information </Col>
+                            <Col className="list" onClick={this.HandlePurchaseHistory}> Purchase history</Col>
+                            <Col className="list" onClick={this.HandleServicesUsage}> Services usage</Col>
+                        </Row>
+                    </Col>
+                </Row>
 
-            <div style={{border:"1px solid black", width:"100%", height:"100%"}}>
-                { this.Display()}
+                <Row style={{ border: "1px solid black", width: "100%", height: "100%" }}>
+                    {this.Display()}
+                </Row>
             </div>
-        </div>
         )
     }
 }
