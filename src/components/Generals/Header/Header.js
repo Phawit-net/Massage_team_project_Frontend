3a5b35dcf1bbac2667386c65bdd604f89e6060fd
Phawit-net <neztle.net@gmail.com>
2020-01-22 09:51:21 +0700

    fix conflict

--- src/components/Generals/Header/Header.js
+++ src/components/Generals/Header/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Row, Col, Card, Menu, Drawer, Button } from 'antd'
 import { Link } from 'react-router-dom'
-import Logo from '../../../picture/NuadThaiLogo.png'
 import styles from './Header.module.css'
 import LoginModal from './LoginModal'
 import UserDropdown from './UserDropdown'
@@ -74,7 +73,7 @@ class Header extends Component {
             <Card style={{ backgroundColor: '#f1e6b2', opacity: '0.8',height:'140px' }} bodyStyle={{ padding: '0' }}>
                 <Row type='flex' justify='center' align='middle'>
                     <Col xs={19} md={5} lg={10} xl={10}>
-                   <Link to ='/home'><img src={Logo} alt='Logo' style={{ width: '150px', height: '150px' }} /></Link>     
+                   <Link to ='/home'><img src='NuadThaiLogo.png' alt='Logo' style={{ width: '150px', height: '150px' }} /></Link>     
                     </Col>
                     <Col xs={5} md={19} lg={12} xl={10}>
                         <Row>
