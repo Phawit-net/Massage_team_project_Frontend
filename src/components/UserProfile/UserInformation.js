ad3142d6cd1f666084b61838d95df1695876cf0e
bebiiez <natthalada.p@gmail.com>
2020-01-28 11:20:02 +0700

    Edit: shop&userProfile

--- src/components/UserProfile/UserInformation.js
+++ src/components/UserProfile/UserInformation.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Col, Form } from "antd";
+import { Row, Col, Form, Icon } from "antd";
 import Axios from "../../config/axios.setup";
 
 export default class UserInformation extends Component {
@@ -49,28 +49,28 @@ export default class UserInformation extends Component {
       <Row type="flex" justify="center" align="top">
         <Col span={22}>
           <Row type="flex" justify='start'>
-            <Col><h1>Personal information</h1></Col>
+            <Col><h1><Icon type="info-circle" /> Personal information</h1></Col>
           </Row>
           <Row>
             <Form {...formItemLayout}>
-              <Form.Item label="Username " style={{ marginTop: "0", marginBottom: "0" }}>
-                <h4>{this.state.Username}</h4>
+              <Form.Item label="Username " style={{ marginTop: "0", marginBottom: "0"}}>
+                <span style={{paddingLeft: '10px'}}>{this.state.Username}</span>
               </Form.Item>
 
               <Form.Item label="Firstname " style={{ marginTop: "0", marginBottom: "0" }}>
-                <h4>{this.state.Firstname}</h4>
+                <span style={{paddingLeft: '10px'}}>{this.state.Firstname}</span>
               </Form.Item>
 
               <Form.Item label="Lastname " style={{ marginTop: "0", marginBottom: "0" }}>
-                <h4>{this.state.Lastname}</h4>
+                <span style={{paddingLeft: '10px'}}>{this.state.Lastname}</span>
               </Form.Item>
 
               <Form.Item label="Email " style={{ marginTop: "0", marginBottom: "0" }}>
-                <h4>{this.state.Email}</h4>
+                <span style={{paddingLeft: '10px'}}>{this.state.Email}</span>
               </Form.Item>
 
               <Form.Item label="Tel " style={{ marginTop: "0", marginBottom: "0" }}>
-                <h4>{this.state.Tel}</h4>
+                <span style={{paddingLeft: '10px'}}>{this.state.Tel}</span>
               </Form.Item>
 
             </Form>
